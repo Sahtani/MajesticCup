85a4d438baeb17c6fa015b8e2f85b11c57f626cb
Sahtani <soumia.sahtani@gmail.com>
2024-12-11 16:13:30 +0100

    initial project

---
+++ src/test/java/com/youcode/majesticcup/MajesticCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.majesticcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajesticCupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
